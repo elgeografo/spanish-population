829ae77d9bf1602d3cea4990da4e832cf87e1876
lizquierdo@srmconsulting.es <lizquierdo@srmconsulting.es>
2017-06-25 11:51:08 +0200

    Mejorado el script para crear la cartografía de Álava

--- INE/manage-municipios-alava.js
+++ INE/manage-municipios-alava.js
@@ -1,6 +1,18 @@
+/**
+ * Script de ejemplo para crear un geojson nuevo conteniendo solo los municipios de Álva
+ */
+
+
 var fs = require('fs');
 var path = require('path');
 
+/**
+ *
+ * Funcion genérica que clona un objeto
+ *
+ * @param obj - Objeto a clonar
+ * @returns {*} - Objeto clonado
+ */
 function clone(obj) {
     if (null == obj || "object" != typeof obj) return obj;
     var copy = obj.constructor();
@@ -10,26 +22,23 @@ function clone(obj) {
     return copy;
 }
 
-
+// Declaramos las variables y creamos el directorio donde volcaremos la cartografía
 var fileMunicipios = '/media/luis/data/kassandra/carto/municpios/Municipios_ETRS89_30N/Municipios_ETRS89_30N_A.json';
 var dirMunicipios = path.dirname(fileMunicipios);
 var output = path.join(dirMunicipios,'output01');
 fs.mkdir(output);
 
-
+// Leémos el fichero geojson con todos los municipios
 var json = JSON.parse(fs.readFileSync(fileMunicipios,'utf8'));
 
+// filtramos las features a todas aequellas que tengan el código de provincia 01, es decir, las de Álava
 var features = json.features.filter(function(feature){
     return feature.properties.Cod_Prov == "01";
 })
 
+// clonamos y sustituímos las features originales (conteniendo todos los municipios de España) con las filtradas. Conteniendo solo las de Álava.
 var jsonAlava = clone(json);
 jsonAlava.features = features;
 
+// grabamos en un fichero geojson
 fs.writeFileSync(path.join(output,'M01.geojson'),JSON.stringify(jsonAlava));
-
-
-
-
-
-var a = 1;
